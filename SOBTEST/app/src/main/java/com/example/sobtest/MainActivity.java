ebb871bef5a41c3448c739727da48ffca478dc4f
dtoto669131 <67650641+dtoto669131@users.noreply.github.com>
2020-07-16 06:24:10 -0700

    Add files via upload

---
+++ SOBTEST/app/src/main/java/com/example/sobtest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.sobtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
